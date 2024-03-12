db3d061de349f093fe617e29616d69d1043cda6a
Yehor <20585619+satelllte@users.noreply.github.com>
2024-03-12 13:39:07 +0200

    resizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,2 @@
 import {Raytracer} from '@/components/Raytracer';
-
-export default function HomePage() {
-  return (
-    <div className='absolute inset-0 flex flex-col gap-4 p-6'>
-      <h1 className='text-center text-3xl'>WebGPU raytracer</h1>
-      <div className='relative flex-1 border border-white'>
-        <Raytracer />
-      </div>
-    </div>
-  );
-}
+export default Raytracer;
--- src/components/Raytracer/Raytracer.tsx
+++ src/components/Raytracer/Raytracer.tsx
@@ -1,125 +1,157 @@
 'use client';
 import {useEffect, useRef} from 'react';
-import {useWindowSize} from 'usehooks-ts';
 import shaderWgsl from './shader.wgsl';
 
 export function Raytracer() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const {width, height} = useWindowSize();
-
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
-    const scale = Math.max(window.devicePixelRatio, 1);
-    canvas.width = Math.floor(width * scale);
-    canvas.height = Math.floor(height * scale);
-  }, [width, height]);
-
-  useEffect(() => {
-    const run = async () => {
-      const canvas = canvasRef.current;
-      if (!canvas) return;
-
-      const {gpu} = navigator;
-      if (!gpu) {
-        showAlert('WebGPU is not supported in this browser');
-        return;
-      }
-
-      const context = canvas.getContext('webgpu');
-      if (!context) {
-        showAlert('Failed to get WebGPU context');
-        return;
-      }
-
-      const adapter = await gpu.requestAdapter();
-      if (!adapter) {
-        showAlert('Failed to request WebGPU adapter');
-        return;
-      }
-
-      const device = await adapter.requestDevice();
-
-      const gpuPreferredCanvasFormat = gpu.getPreferredCanvasFormat();
-
-      context.configure({
-        device,
-        format: gpuPreferredCanvasFormat,
-        alphaMode: 'premultiplied',
-      });
-
-      const shaderModule = device.createShaderModule({code: shaderWgsl});
-
-      // prettier-ignore
-      const vertices = new Float32Array([
-        // Position<vec4f> (XYZW)
-        -1.0,  1.0, 0.0, 1.0,
-        -1.0, -1.0, 0.0, 1.0,
-         1.0,  1.0, 0.0, 1.0,
-         1.0,  1.0, 0.0, 1.0,
-        -1.0, -1.0, 0.0, 1.0,
-         1.0, -1.0, 0.0, 1.0,
-      ]);
-
-      const verticesBuffer = device.createBuffer({
-        size: vertices.byteLength,
-        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST, // eslint-disable-line no-bitwise
-      });
-
-      device.queue.writeBuffer(verticesBuffer, 0, vertices, 0, vertices.length);
-
-      const renderPipeline = device.createRenderPipeline({
-        layout: 'auto',
-        primitive: {topology: 'triangle-list'},
-        vertex: {
-          module: shaderModule,
-          entryPoint: 'vertex_main',
-          buffers: [
-            {
-              arrayStride: 16,
-              stepMode: 'vertex',
-              attributes: [
-                {shaderLocation: 0, offset: 0, format: 'float32x4'}, // Position
-              ],
-            },
-          ] as const satisfies Iterable<GPUVertexBufferLayout>,
-        },
-        fragment: {
-          module: shaderModule,
-          entryPoint: 'fragment_main',
-          targets: [
-            {format: gpuPreferredCanvasFormat},
-          ] as const satisfies Iterable<GPUColorTargetState>,
-        },
-      });
-
-      const commandEncoder = device.createCommandEncoder();
-      const passEncoder = commandEncoder.beginRenderPass({
-        colorAttachments: [
-          {
-            clearValue: [0.0, 0.0, 0.0, 1.0],
-            view: context.getCurrentTexture().createView(),
-            loadOp: 'clear',
-            storeOp: 'store',
-          },
-        ] as const satisfies Iterable<GPURenderPassColorAttachment>,
-      });
-
-      passEncoder.setPipeline(renderPipeline);
-      passEncoder.setVertexBuffer(0, verticesBuffer);
-      passEncoder.draw(6);
-      passEncoder.end();
-
-      device.queue.submit([commandEncoder.finish()]);
+    const resizeCanvas = () => {
+      const {width, height} = canvas.getBoundingClientRect();
+      const scale = Math.max(window.devicePixelRatio, 1);
+      canvas.width = Math.floor(width * scale);
+      canvas.height = Math.floor(height * scale);
     };
 
-    void run();
+    resizeCanvas();
+
+    window.addEventListener('resize', resizeCanvas);
+
+    return () => {
+      window.removeEventListener('resize', resizeCanvas);
+    };
   }, []);
 
-  return <canvas ref={canvasRef} className='absolute h-full w-full' />;
+  const render = async () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const {gpu} = navigator;
+    if (!gpu) {
+      showAlert('WebGPU is not supported in this browser');
+      return;
+    }
+
+    const context = canvas.getContext('webgpu');
+    if (!context) {
+      showAlert('Failed to get WebGPU context');
+      return;
+    }
+
+    const adapter = await gpu.requestAdapter();
+    if (!adapter) {
+      showAlert('Failed to request WebGPU adapter');
+      return;
+    }
+
+    const device = await adapter.requestDevice();
+
+    draw({gpu, context, device});
+  };
+
+  return (
+    <div className='absolute inset-0 flex flex-col gap-4 p-6'>
+      <div className='flex flex-col gap-2 sm:flex-row sm:justify-between'>
+        <h1 className='text-2xl sm:text-3xl'>WebGPU raytracer</h1>
+        <button
+          type='button'
+          className='border px-4 py-1 hover:bg-zinc-900 active:bg-zinc-800'
+          onClick={render}
+        >
+          Render
+        </button>
+      </div>
+      <div className='relative flex-1 border border-white'>
+        <canvas ref={canvasRef} className='absolute h-full w-full' />
+      </div>
+    </div>
+  );
 }
+
+const draw = ({
+  gpu,
+  context,
+  device,
+}: {
+  gpu: GPU;
+  context: GPUCanvasContext;
+  device: GPUDevice;
+}): void => {
+  const gpuPreferredCanvasFormat = gpu.getPreferredCanvasFormat();
+
+  context.configure({
+    device,
+    format: gpuPreferredCanvasFormat,
+    alphaMode: 'premultiplied',
+  });
+
+  const shaderModule = device.createShaderModule({code: shaderWgsl});
+
+  // prettier-ignore
+  const vertices = new Float32Array([
+    // Position<vec4f> (XYZW)
+    -1.0,  1.0, 0.0, 1.0,
+    -1.0, -1.0, 0.0, 1.0,
+     1.0,  1.0, 0.0, 1.0,
+     1.0,  1.0, 0.0, 1.0,
+    -1.0, -1.0, 0.0, 1.0,
+     1.0, -1.0, 0.0, 1.0,
+  ]);
+
+  const verticesBuffer = device.createBuffer({
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST, // eslint-disable-line no-bitwise
+  });
+
+  device.queue.writeBuffer(verticesBuffer, 0, vertices, 0, vertices.length);
+
+  const renderPipeline = device.createRenderPipeline({
+    layout: 'auto',
+    primitive: {topology: 'triangle-list'},
+    vertex: {
+      module: shaderModule,
+      entryPoint: 'vertex_main',
+      buffers: [
+        {
+          arrayStride: 16,
+          stepMode: 'vertex',
+          attributes: [
+            {shaderLocation: 0, offset: 0, format: 'float32x4'}, // Position
+          ],
+        },
+      ] as const satisfies Iterable<GPUVertexBufferLayout>,
+    },
+    fragment: {
+      module: shaderModule,
+      entryPoint: 'fragment_main',
+      targets: [
+        {format: gpuPreferredCanvasFormat},
+      ] as const satisfies Iterable<GPUColorTargetState>,
+    },
+  });
+
+  const commandEncoder = device.createCommandEncoder();
+  const passEncoder = commandEncoder.beginRenderPass({
+    colorAttachments: [
+      {
+        clearValue: [0.0, 0.0, 0.0, 1.0],
+        view: context.getCurrentTexture().createView(),
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ] as const satisfies Iterable<GPURenderPassColorAttachment>,
+  });
+
+  passEncoder.setPipeline(renderPipeline);
+  passEncoder.setVertexBuffer(0, verticesBuffer);
+  passEncoder.draw(6);
+  passEncoder.end();
+
+  device.queue.submit([commandEncoder.finish()]);
+};
 
 const showAlert = (message: string): void => {
   alert(message); // eslint-disable-line no-alert
